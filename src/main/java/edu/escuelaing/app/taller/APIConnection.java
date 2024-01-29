df6b09dde190cb24bcc4f57ca17bf73268b2aae8
nicolas.ariza241@gmail.com <nicolas.ariza241@gmail.com>
2024-01-29 16:50:40 -0500

    new methods and tests added, regenerated javadoc

--- src/main/java/edu/escuelaing/app/taller/APIConnection.java
+++ src/main/java/edu/escuelaing/app/taller/APIConnection.java
@@ -15,7 +15,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * 
  * The class utilizes a ConcurrentHashMap for caching responses to avoid redundant API calls
  * for the same movie title.
- * @Author Nicolas Ariza barbosa
+ * 
+ * @Author Nicolas Ariza Barbosa
  */
 public class APIConnection {
     private String userQuery;
@@ -26,17 +27,48 @@ public class APIConnection {
 
     /**
      * Sets the user query to the specified movie title.
+     * 
      * @param newQuery The movie title to set as the user query.
      */
-    public void setQuery(String newQuery){
+    public void setUserQuery(String newQuery){
         this.userQuery = newQuery;
     }
 
     /**
+     * Retrieves the current user query.
+     * 
+     * @return The current user query.
+     */
+    public String getUserQuery(){
+        return this.userQuery;
+    }
+
+    /**
+     * Stores the movie data in the cache.
+     * 
+     * @param movieName  The name of the movie to use as the cache key.
+     * @param movieData  The movie data to store in the cache.
+     */
+    public void storeMovieData(String movieName, StringBuffer movieData){
+        cache.put(movieName, movieData);
+    }
+
+    /**
+     * Retrieves movie data from the cache.
+     * 
+     * @param movieName The name of the movie to retrieve from the cache.
+     * @return The movie data stored in the cache.
+     */
+    public StringBuffer getMovieData(String movieName){
+        return cache.get(movieName);
+    }
+
+    /**
      * Makes a GET request to the OMDB API using the specified user query and handles the response.
-     * @param outPut PrintWriter object to send the API response to the client.
+     * 
+     * @param outPut           PrintWriter object to send the API response to the client.
      * @param sResponseHeaders Object to manage HTTP response headers.
-     * @param sResponseData Object to manage HTTP response data.
+     * @param sResponseData    Object to manage HTTP response data.
      * @throws IOException If an I/O error occurs while making the API request.
      */
     public void getRequest(PrintWriter outPut, HTTPResponseHeaders sResponseHeaders, HTTPResponseData sResponseData) throws IOException{
@@ -45,9 +77,9 @@ public class APIConnection {
         sResponseHeaders.setContentType("json");
         outPut.println(sResponseHeaders.OKResponse());
 
-        if(cache.get(this.userQuery) == null){ // Check if the movie name is alredy in cache
+        if(cache.get(getUserQuery()) == null){ // Check if the movie name is already in cache
             // Establishes a connection to the OMDB API
-            URL obj = new URL(GET_URL + "&t=" + this.userQuery);
+            URL obj = new URL(GET_URL + "&t=" + getUserQuery());
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("User-Agent", USER_AGENT);
@@ -71,7 +103,7 @@ public class APIConnection {
                     outPut.println(sResponseData.getJSONErrorMessage());
                 }else{
                     // Stores the API response in the cache
-                    cache.put(this.userQuery, response);
+                    storeMovieData(this.userQuery, response);
                     // Sends the API response to the client
                     outPut.println(response.toString());
                 }
@@ -81,7 +113,7 @@ public class APIConnection {
             }
         }else{
             // Sends the cached API response to the client
-            outPut.println(cache.get(this.userQuery).toString());
+            outPut.println(getMovieData(this.userQuery).toString());
         }
     }
 }
